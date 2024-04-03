472b914f7f2b62dce69248634c3bb704f17056e6
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-04-03 20:00:02 -0300

    chore: import card in forecast component

--- forecasting/src/components/forecast/Forecast.tsx
+++ forecasting/src/components/forecast/Forecast.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
-import './Forecast.css';
+import React from "react";
+import "./Forecast.css";
+import Card from "../card/Card.tsx";
 
 export default function Forecast() {
   return (
-    <div>Forecast</div>
-  )
+    <div>
+      <Card value={0} />
+      <Card value={1} />
+      <Card value={2} />
+      <Card value={3} />
+    </div>
+  );
 }
