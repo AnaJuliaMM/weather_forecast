4b7c23e30cc6aa6dd70d3f329a60b57d959e2be2
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-04-03 20:01:02 -0300

    chore: add all others components inside app

--- forecasting/src/App.js
+++ forecasting/src/App.js
@@ -1,9 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Search from './components/search/Search.tsx';
+import Temperature from './components/temperature/Temperature.tsx';
+import Forecast from './components/forecast/Forecast.tsx';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1 className='headerTitle'>Hello World</h1>
+      <Search/>
+      <Temperature/>
+      <Forecast/>
+    </>
+
   );
 }
 
