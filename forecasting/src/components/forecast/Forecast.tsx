784222ef33cf382bd45088a3d43d24f66a27ae51
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-04-03 22:35:07 -0300

    chore: update cards props in father component

--- forecasting/src/components/forecast/Forecast.tsx
+++ forecasting/src/components/forecast/Forecast.tsx
@@ -4,11 +4,14 @@ import Card from "../card/Card.tsx";
 
 export default function Forecast() {
   return (
-    <div>
-      <Card value={0} />
-      <Card value={1} />
-      <Card value={2} />
-      <Card value={3} />
+    <div className="forecastDiv">
+      <Card weekday='Segunda' temperature={0} weather_condition='Nublado'/>
+      <Card weekday='Terça' temperature={1} weather_condition='Nublado'/>
+      <Card weekday='Quarta' temperature={2} weather_condition='Nublado'/>
+      <Card weekday='Quinta' temperature={3} weather_condition='Nublado'/>
+      <Card weekday='Sexta' temperature={4} weather_condition='Nublado'/>
+      <Card weekday='Sábado' temperature={5} weather_condition='Nublado'/>
+      <Card weekday='Domingo' temperature={6} weather_condition='Nublado'/>
     </div>
   );
 }
