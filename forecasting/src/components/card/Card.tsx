d4ebc8e701bdcfb5cdf47ec53799a36367d007f7
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-04-03 22:34:40 -0300

    chore: update props

--- forecasting/src/components/card/Card.tsx
+++ forecasting/src/components/card/Card.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import "./Card.css";
 
-export default function Card({ value }) {
-  return <div>{value}</div>;
+export default function Card({ weekday, temperature, weather_condition  }) {
+  return (
+    <div className="cardDiv">
+      <h3>{weekday}</h3>
+      <h2>{temperature}°C</h2>
+      <h4>{weather_condition}</h4>
+    </div>
+  );
 }
