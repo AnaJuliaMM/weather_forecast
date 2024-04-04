e6f79e054d540a5a9f69ff95e0d726c455fb31a5
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-04-03 22:35:32 -0300

    chore: add request to api

--- forecasting/src/components/search/Search.tsx
+++ forecasting/src/components/search/Search.tsx
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Search.css";
 
+const apiKey = "8290a9f4b4302fad3f55ebc9b4c1cf79";
+
 export default function Search() {
+  const [value, setValue] = useState('');
+  const [cityName, setCityName] = useState('')
+
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+
+  const handleInputText = (event) => {
+    setValue(event.target.value);
+  };
+
+  const handleClick = () => {
+    setCityName(value)  
+    request()
+  };
+
+  const request = () => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="searchDiv">
       <span className="inputSpan">
-        <input type="text" />
+        <input type="text" onChange={(event) => handleInputText(event)} />
       </span>
       <span className="inputSpan">
-        <input type="button" value="Buscar" />
+        <input type="button" value="Buscar" onClick={handleClick} />
       </span>
     </div>
   );
